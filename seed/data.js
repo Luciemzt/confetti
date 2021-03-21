de65b836148ac66bd9c75c448805fc4666d11be1
Luciemzt <lucie.mzt@gmail.com>
2021-03-21 19:16:07 +0100

    new images

--- seed/data.js
+++ seed/data.js
@@ -4,29 +4,28 @@ const places = [
     description: "Chill & nice",
     adress: "Carrer d'en Rauric, 9, 08002 Barcelona",
     type: "Coktail-Bar", 
-    imageURL: 'https://bs.https://offloadmedia.feverup.com/barcelonasecreta.com/wp-content/uploads/2018/10/09111349/0MqPutZ3gZOOBbTXtKAWaZ7xHiZkxukD9DspvrlnKklOIzA0-QcBxT8owhpeiKkcI_nm0_ooonCzOnTQMJs7ZsKJcGKCiHgq8B7_xu6c7_ID2f43UZallM58kjdnNUSjQRvDwl6syV9gv-SAlhxb9q53F4h2_Z-6Mi525jpaCP7wq_V9RQs2048.jpg.org/image/o/479632a47f3f989d1f29ddf04fd5aa49038430eb'
+    imageURL: "https://media-cdn.tripadvisor.com/media/photo-s/0b/0c/ba/f2/first-cocktail-bar-barcelona.jpg"
 },
 {
     name: "Ocaña",
     description: "Chill & nice",
     adress: " Plaça Reial, 13-15, 08002 Barcelona",
     type: "Bar-Restaurant", 
-    imageURL: 'https://bs.flhttps://barcelonanavigator.com/wp-content/uploads/2015/10/Oca%C3%B1a-2.jpgoristic.org/image/o/479632a47f3f989d1f29ddf04fd5aa49038430eb'
+    imageURL: "https://barcelonanavigator.com/wp-content/uploads/2015/10/Oca%C3%B1a-Terrace.jpg"
 },
 {
     name: "Paradiso",
     description: "Chill & nice",
     adress: "Carrer de Rera Palau, 4, 08003 Barcelona",
     type: "Coktail-Bar", 
-    imageURL: 'https://bs.https.org/image/o/479632a47f3f989d1f29ddf04fd5aa49038430eb'
+    imageURL: "https://www.lavanguardia.com/files/article_main_microformat/uploads/2017/10/12/5e997b4746e9b.jpeg"
 },
 {
     name: "El Andorra",
     description: "Chill & nice",
     adress: "Carrer de Sant Pere Més Alt, 74, 08003 Barcelona",
     type: "Tapas", 
-    imageURL: 'https://bs.https://www.google.com/maps/place/El+Andorra/@41.3892483,2.1783001,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipMW2u8eM9NR8IuMnuZA_mCi5JrLlel-EU1Mmqr9!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMW2u8eM9NR8IuMnuZA_mCi5JrLlel-EU1Mmqr9%3Dw203-h284-k-no!7i1200!8i1680!4m5!3m4!1s0x12a4a2fb6a349485:0xa8ecd73cb0826760!8m2!3d41.3892253!4d2.178349?hl=fr#.org/image/o/479632a47f3f989d1f29ddf04fd5aa49038430eb'
-},
-]
+    imageURL: "https://media-cdn.tripadvisor.com/media/photo-s/04/31/dd/5c/bar-restaurante-andorra.jpg"
+}
 
 module.exports = {places}; 
